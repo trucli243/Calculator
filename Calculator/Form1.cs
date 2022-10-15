f4c2a5f2c83b71ef992123f2202c279512c7a788
trucli243 <trucly2403022gmail.com>
2022-10-15 14:42:34 +0700

    Them dau bt tru

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -24,5 +24,13 @@ namespace Calculator
             double c = a + b;
             textBox3.Text = c.ToString();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(textBox1.Text);
+            double b = double.Parse(textBox2.Text);
+            double c = a - b;
+            textBox3.Text = c.ToString();
+        }
     }
 }
